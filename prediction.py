6f4fe34b2a9a924bef58ee340892b83ad9a36b55
Vansh Mhatre <vanshmhatre342006@gmail.com>
2026-02-05 23:18:20 +0530

    feat : created prediction logic

---
+++ prediction.py
@@ -0,0 +1,49 @@
+
+import torch
+from model import Decoder_block
+from preprocessing import sp ,vocab_size
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+block_size = 8
+
+model = Decoder_block(vocab_size , embed_dim=128 , n_head=16).to(device)
+model.load_state_dict(torch.load('Mini_gpt/saved_model_and_files/trained_model_25k.pth'))
+
+
+model.eval()
+maxlength = 50
+text = 'the game was really good '
+
+
+
+def generate(text, maxlength):
+    
+    ids = sp.Encode(text , out_type= int)
+        
+    decoder_input =torch.tensor( ids ,device=device).unsqueeze(0)
+    print(decoder_input.shape)
+        
+    for _ in range(maxlength-1):
+        
+        idx_cond = decoder_input[:, -block_size:]
+        logits = model(idx_cond)
+        
+        next_token = logits[: ,-1 ,:]
+        
+        next_token_tensor = torch.argmax(next_token , dim=-1).unsqueeze(dim=1)       
+        # print(next_token_tensor.shape)
+        decoder_input= torch.cat( (decoder_input , next_token_tensor),dim=1)
+    
+    token_ids = decoder_input[0].tolist()
+    
+    return sp.Decode(token_ids)
+        
+        
+        
+output = generate(text , maxlength)
+
+print(output)
+        
+        
+    
